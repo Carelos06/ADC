6c9f1aa14dcc85269e1d15649914bcbefe5028fa
Carlos Andrés Valencia Vargas <cav4956@gmail.com>
2022-09-26 20:44:27 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $link = mysqli_connect("localhost", "papu1", "") or die ("<h2>Error</h2>");;
+    $link = mysqli_connect("localhost", "papu1", "2cR2~K|&?jbD^GWf") or die ("<h2>Error</h2>");;
     $bd = mysqli_select_db("id19629147_citas", $link) or die ("<h2>Error 2</h2>");
     
     $correo=$_POST['correo'];

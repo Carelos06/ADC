ce2f99b2c60932282d52cb6bc3fa6dce14d1ccfc
Carlos Andrés Valencia Vargas <cav4956@gmail.com>
2022-09-26 22:27:28 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -9,7 +9,7 @@
 
     $req(strlen($correo), strlen($contraseña));
 
-    mysqli_query($link, "INSERT INTO USUARIO VALUES (NULL, '$correo', '$contraseña', 'Activo', 1)") or die ("<h2>Error 3</h2>");
+    mysqli_query($link, "INSERT INTO USUARIO VALUES ('', '$correo', '$contraseña', 'Activo', 1)") or die ("<h2>Error 3</h2>");
 
 echo'
 <h2>hecho</h2>

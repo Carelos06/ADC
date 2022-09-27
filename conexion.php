976a270f7f09f1666934e31d672481a7bb83bdd6
Carlos Andrés Valencia Vargas <cav4956@gmail.com>
2022-09-27 16:39:24 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -2,14 +2,14 @@
     $link = mysqli_connect("localhost", "id19629147_papu1", "2cR2~K|&?jbD^GWf") or die ("<h2>Error</h2>");
     $bd = mysqli_select_db($link, "id19629147_citas") or die ("<h2>Error 2</h2>");
     
-    $base=["usuario"];
+    $base=["USUARIO"];
 
     $correo=$_POST['correo'];
     $contraseña=$_POST['contraseña'];
 
     $req(strlen($correo)* strlen($contraseña)) or die ("No se han llenado todos los campos");
 
-    mysqli_query($link, "INSERT INTO USUARIO VALUES (null, '$correo', '$contraseña', 'Activo', 1)") or die ("<h2>Error 3</h2>");
+    mysqli_query($link, "INSERT INTO USUARIO (idUsuario, correoUsuario, passwordUsuario, estadoUsuario, IdRolKF) VALUES (null, '$correo', '$contraseña', 'Activo', 1)") or die ("<h2>Error 3</h2>");
 
 echo'
 <h2>hecho</h2>
